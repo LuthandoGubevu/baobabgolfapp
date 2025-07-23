71feb396421f5fc8a19c96a36cd74b6f94f90cc7
Keegan Baobabb <keeganbaobabb@gmail.com>
2025-07-23 11:15:27 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/hole/[holeNumber]/page.tsx
+++ src/app/hole/[holeNumber]/page.tsx
@@ -1,4 +1,3 @@
-import { Header } from '@/components/Header';
 import HoleScorecard from './HoleScorecard';
 import { TOTAL_HOLES } from '@/lib/constants';
 import { notFound } from 'next/navigation';
@@ -8,14 +7,6 @@ type HolePageProps = {
     holeNumber: string;
   };
 };
-
-export function generateStaticParams() {
-  // We can't know at build time if it will be 9 or 18 holes,
-  // but we must generate static pages. We'll generate for the max.
-  return Array.from({ length: TOTAL_HOLES }, (_, i) => ({
-    holeNumber: (i + 1).toString(),
-  }));
-}
 
 export default function HolePage({ params }: HolePageProps) {
   const holeNumber = parseInt(params.holeNumber, 10);
