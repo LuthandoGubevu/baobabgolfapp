70ec549d54710f87ab53418da8758f1eeac3e664
Keegan Baobabb <keeganbaobabb@gmail.com>
2025-07-23 10:45:07 +0000

    I see nothing in the applications

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet"></link>
+        <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#e4002b" />
       </head>
       <body className="font-body antialiased min-h-screen bg-background flex flex-col">
